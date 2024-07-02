5aaff58d6548707aa4cc05a530d10625487c7c10
Gadiraju Yasaswini Devi <g.yasaswini2023@gmail.com>
2024-07-02 20:42:48 +0530

    Added a basic maze and set it to display on the Pygame window

--- ghost-dodgers.py
+++ ghost-dodgers.py
@@ -1,19 +1,45 @@
 import pygame
 
+maze = [
+    "#########################################",
+    "#                                       #",
+    "# ########## ######### ################ #",
+    "# #        # #       #              # # #",
+    "# #  ####### # ##### ####### ###### # # #",
+    "# #  #       #     #       # #      # # #",
+    "# #  # ############### ##### # ###### # #",
+    "# #  #                 #     # #      # #",
+    "# #  ######## ########## ##### # ###### #",
+    "# #                            #        #",
+    "#########################################"
+]
+
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 400
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+CELL_SIZE = 10
+
 pygame.init()
 
-SIZE= (700, 500)
-screen = pygame.display.set_mode(SIZE)
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Recreating Pacman")
 
 running = True
-
-clock = pygame.time.Clock()
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
+    screen.fill(WHITE)
+
+    for y, row in enumerate(maze):
+        for x, cell in enumerate(row):
+            color = BLACK if cell == '#' else WHITE
+            rect = pygame.Rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+            pygame.draw.rect(screen, color, rect)
+
     pygame.display.flip()
 
 pygame.quit()
