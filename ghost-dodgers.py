e7dc203a3176efd17e82af5b833a2424ae2d52d7
Gadiraju Yasaswini Devi <g.yasaswini2023@gmail.com>
2024-07-06 17:38:55 +0530

    Added separate functions for drawing walls, pellets and moving player

--- ghost-dodgers.py
+++ ghost-dodgers.py
@@ -3,18 +3,16 @@ import pygame
 maze = [
     "#########################################",
     "#                                       #",
-    "# ########## ######### ################ #",
-    "# #        # #       #              # # #",
-    "# #  ####### # ##### ####### ###### # # #",
-    "# #  #       #     #       # #      # # #",
-    "# #  # ############### ##### # ###### # #",
-    "# #  #                 #     # #      # #",
-    "# #  ######## ########## ##### # ###### #",
-    "# #                            #        #",
+    "# ########## ######### ############## # #",
+    "# #          #       #              # # #",
+    "# # ######## # ##### ####### ###### # # #",
+    "# # #        #     #       # #      # # #",
+    "# # #  ############### ##### # ###### # #",
+    "# # #                  #     # #      # #",
+    "# # ######## ########### ##### # ###### #",
+    "#                                       #",
     "#########################################"
 ]
-
-#maze = [list(row.replace(" ", ".")) for row in maze]
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 400
@@ -29,36 +27,51 @@ pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Recreating Pacman")
 
+def draw_maze():
+    screen.fill(BLACK)
+    for y, row in enumerate(maze):
+        for x, cell in enumerate(row):
+            if cell == '#': 
+                draw_wall(x, y)
+            else:
+                draw_pellet(x, y)
+    pygame.display.flip()
+
+def draw_wall(x: int, y: int):
+    rect = pygame.Rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+    pygame.draw.rect(screen, BLUE, rect)
+
+def draw_pellet(x: int, y: int):
+    pygame.draw.circle(screen, WHITE, (x * CELL_SIZE + CELL_SIZE // 2, y * CELL_SIZE + CELL_SIZE // 2), CELL_SIZE // 10)
+
 def update_cell(x: int, y: int):
-    if maze[y][x] == '.':
-        maze[y][x] = ' '
     rect = pygame.Rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
     pygame.draw.rect(screen, BLACK, rect)
+
+def move_player(x: int, y: int):
+    pygame.draw.circle(screen, YELLOW, (x * CELL_SIZE + CELL_SIZE // 2, y * CELL_SIZE + CELL_SIZE // 2), CELL_SIZE // 2)
 
 def run_game():
     x, y = 1, 1
     running = True
     draw_maze()
+    move_player(x, y)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN:
                 update_cell(x, y)
-                x += 1
-                pygame.draw.circle(screen, YELLOW, (x * CELL_SIZE + CELL_SIZE // 2, y * CELL_SIZE + CELL_SIZE // 2), CELL_SIZE // 2)                
-            pygame.display.flip()
-
-def draw_maze():
-    screen.fill(BLACK)
-    for y, row in enumerate(maze):
-        for x, cell in enumerate(row):
-            if cell == '#': 
-                rect = pygame.Rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
-                pygame.draw.rect(screen, BLUE, rect)
-            else:
-                pygame.draw.circle(screen, WHITE, (x * CELL_SIZE + CELL_SIZE // 2, y * CELL_SIZE + CELL_SIZE // 2), CELL_SIZE // 10)
-    pygame.display.flip()
+                if event.key == pygame.K_LEFT:
+                    x -= 1
+                elif event.key == pygame.K_RIGHT:
+                    x += 1
+                elif event.key == pygame.K_UP:
+                    y -= 1
+                elif event.key == pygame.K_DOWN:
+                    y += 1
+                move_player(x, y)
+        pygame.display.flip()
 
 run_game()
 pygame.quit()
