820a261b8fc6acb5eb1f2b61220009bc0bb7518d
Disha Singla <dishasingla2005@gmail.com>
2024-07-06 22:07:55 +0530

    ghost class initialised

---
+++ ghosts.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Ghost:
+    def __init__(self, x_coord, y_coord, target, speed, img, direct, dead, box, id):
+        self.x_pos = x_coord
+        self.y_pos = y_coord
+        self.center_x = self.x_pos + 22
+        self.center_y = self.y_pos + 22
+        self.target = target
+        self.speed = speed
+        self.img = img
+        self.direction = direct
+        self.dead = dead
+        self.in_box = box
+        self.id = id
+        self.turns, self.in_box = self.check_collisions()
+        self.rect = self.draw()
+
+        
