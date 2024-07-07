de8697b6aee29b900d13230a278374df5dcab666
Gadiraju Yasaswini Devi <g.yasaswini2023@gmail.com>
2024-07-07 14:16:08 +0530

    Initialized the maze grid. The maze consists of (rows x columns) cells and will be constructed starting at the cell indexed (ix, iy)

--- maze.py
+++ maze.py
@@ -3,3 +3,10 @@ class Cell:
     def __init__(self, x: int, y: int):
         self.x, self.y = x, y
         self.walls = {'N': True, 'S': True, 'E': True, 'W': True}
+
+class Maze:
+
+    def __init__(self, rows: int, colums: int, ix: int, iy: int):
+        self.rows, self.colums = rows, columns
+        self.ix, self.iy = ix, iy
+        self.maze_map = [[Cell(x, y) for y in range(columns)] for x in range(rows)]
