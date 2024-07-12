40be7e141bff572a717761138e3ecbe20f1d5d26
Disha Singla <dishasingla2005@gmail.com>
2024-07-12 17:30:15 +0530

    added ghosts position speed in constants

--- constants.py
+++ constants.py
@@ -15,3 +15,16 @@ PACMAN_START_Y = 1
 width, height = 21, 21
 ghost_house_x, ghost_house_y = 9, 9
 ghost_house_width, ghost_house_height = 3, 3
+ghost1_x = 56
+ghost1_y = 58
+ghost1_direction = 0
+ghost2_x = 440
+ghost2_y = 388
+ghost2_direction = 2
+ghost3_x = 440
+ghost3_y = 438
+ghost3_direction = 2
+ghost4_x = 440
+ghost4_y = 438
+ghost4_direction = 2
+
