ec888dd275e23408d94c04cb87c8ebcd0a8342dd
Krishita Garg <krishitagarg@gmail.com>
2024-07-12 16:08:37 +0530

    Removed unnecessary lines

--- constants.py
+++ constants.py
@@ -8,13 +8,10 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
 CELL_SIZE = 15
-<<<<<<< HEAD
 nx, ny = 15, 10
 ix, iy = 0, 0
 PACMAN_START_X = 1
 PACMAN_START_Y = 1
-=======
 width, height = 21, 21
 ghost_house_x, ghost_house_y = 9, 9
 ghost_house_width, ghost_house_height = 3, 3
->>>>>>> d14c1f0dc20b4be7869cf5d72b31d3e68d7ac447
