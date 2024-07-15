a60638d4c83850d30e3eef2088cfd18bed71cab0
Krishita Garg <krishitagarg@gmail.com>
2024-07-15 13:43:03 +0530

    Changed the color function to a constant being imported from constants file

--- cells.py
+++ cells.py
@@ -1,4 +1,5 @@
 import pygame
+from constants import *
 
 class Cell(pygame.sprite.Sprite):
 	def __init__(self, row, col, length, width):
@@ -8,8 +9,7 @@ class Cell(pygame.sprite.Sprite):
 		self.id = (row, col)
 		self.abs_x = row * self.width
 		self.abs_y = col * self.height
-
-		self.rect = pygame.Rect(self.abs_x,self.abs_y,self.width,self.height)
+		self.rect = pygame.Rect(self.abs_x, self.abs_y, self.width, self.height)
 
 	def update(self, screen):
-		pygame.draw.rect(screen, pygame.Color.b, self.rect)
+		pygame.draw.rect(screen, BLUE, self.rect)
