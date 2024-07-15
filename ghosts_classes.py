defb565a8d53898d5a6af4e0e88834bc23ea8370
Krishita Garg <krishitagarg@gmail.com>
2024-07-15 13:31:17 +0530

    Added classes for each of the 4 ghosts

---
+++ ghosts_classes.py
@@ -0,0 +1,23 @@
+import pygame
+from constants import *
+
+ghost1 = pygame.transform.scale(pygame.image.load(f'assets/ghost1_hallowen.png'), (CELL_SIZE, CELL_SIZE))
+ghost2 = pygame.transform.scale(pygame.image.load(f'assets/ghost2_hallowen.png'), (CELL_SIZE, CELL_SIZE))
+ghost3 = pygame.transform.scale(pygame.image.load(f'assets/ghost3_hallowen.png'), (CELL_SIZE, CELL_SIZE))
+ghost4 = pygame.transform.scale(pygame.image.load(f'assets/ghost4_hallowen.png'), (CELL_SIZE, CELL_SIZE))
+
+class Ghost1(Pacman):
+    def __init__(self, x, y, color):
+        super().__init__(x, y, ghost1)
+
+class Ghost2(Pacman):
+    def __init__(self, x, y, color):
+        super().__init__(x, y, ghost2)
+
+class Ghost3(Pacman):
+    def __init__(self, x, y, color):
+        super().__init__(x, y, ghost3)
+
+class Ghost4(Pacman):
+    def __init__(self, x, y, color):
+        super().__init__(x, y, ghost4)
