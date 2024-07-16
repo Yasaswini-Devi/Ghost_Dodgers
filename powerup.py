26fd877d80fa53042198e0ad555ff208ce4de51f
Krishita Garg <krishitagarg@gmail.com>
2024-07-16 21:30:46 +0530

    Added code to load image for power-up

--- powerup.py
+++ powerup.py
@@ -3,7 +3,7 @@ from constants import *
 from pellets import *
 
 class PowerUp(Pellet):
-    def __init__(self, x, y, size, is_power_up=False):
+    def __init__(self, x, y, size, is_power_up = False):
         super().__init__(x, y, size)
         self.is_power_up = is_power_up
-        self.image.fill(RED)
+        self.image = pygame.transform.scale(pygame.image.load('assets/power-up.png'), (size, size))
