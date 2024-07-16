469feba7d366e0a0e6af698c65925541991e54a2
Krishita Garg <krishitagarg@gmail.com>
2024-07-16 21:36:16 +0530

    Changed the color of pellets from white to yellow

--- pellets.py
+++ pellets.py
@@ -5,7 +5,7 @@ class Pellet(pygame.sprite.Sprite):
     def __init__(self, x, y, size):
         super().__init__()
         self.image = pygame.Surface((size, size))
-        self.image.fill(WHITE)
+        self.image.fill(YELLOW)
         self.rect = self.image.get_rect()
         self.rect.center = ((x + 0.5) * CELL_SIZE, (y + 0.5) * CELL_SIZE)
 
