ccb3bfa0bc9873fa88db50d4adcd900266e6a19c
Krishita Garg <krishitagarg@gmail.com>
2024-07-19 01:24:07 +0530

    Added theme as a parameter to each ghost class

--- ghosts_classes.py
+++ ghosts_classes.py
@@ -33,17 +33,17 @@ class Ghost(pygame.sprite.Sprite):
 
 
 class Ghost1(Ghost):
-    def __init__(self, x, y):
+    def __init__(self, x, y, theme):
         super().__init__(x, y, theme.get_ghost_image(0), (x * CELL_SIZE, y * CELL_SIZE))
         
 class Ghost2(Ghost):
-    def __init__(self, x, y):
+    def __init__(self, x, y, theme):
         super().__init__(x, y, theme.get_ghost_image(1), (x * CELL_SIZE, y * CELL_SIZE))
 
 class Ghost3(Ghost):
-    def __init__(self, x, y):
+    def __init__(self, x, y, theme):
         super().__init__(x, y, theme.get_ghost_image(2), (x * CELL_SIZE, y * CELL_SIZE))
 
 class Ghost4(Ghost):
-    def __init__(self, x, y):
+    def __init__(self, x, y, theme):
         super().__init__(x, y, theme.get_ghost_image(3), (x * CELL_SIZE, y * CELL_SIZE))
