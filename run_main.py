e3c45b0cb7bba9ec494f861abccdef37d1eaf1b6
Gadiraju Yasaswini Devi <g.yasaswini2023@gmail.com>
2024-07-19 16:18:53 +0530

    Moving back to the hacker's theme

--- run_main.py
+++ run_main.py
@@ -8,7 +8,7 @@ def main():
     pygame.display.set_caption('Trouble Escapers')
 
     clock = pygame.time.Clock()
-    theme_name = "It's Halloween Time"
+    theme_name = "Let's Hack"
     maze = Maze(screen, theme_name)
 
     running = True
