87a7247ae2ca9c5ab2e1cc6830f8c9213b7db8e3
Gadiraju Yasaswini Devi <g.yasaswini2023@gmail.com>
2024-07-23 15:39:46 +0530

    Corrected a syntax error

--- ghosts_classes.py
+++ ghosts_classes.py
@@ -35,7 +35,7 @@ class Ghost(pygame.sprite.Sprite):
             self.target = random.choice(scatter_targets)
         elif self.mode == 'chase':
             self.target = (maze.player.sprite.rect.x // CELL_SIZE, maze.player.sprite.rect.y // CELL_SIZE)
-        elif mode == 'frightened':
+        elif self.mode == 'frightened':
             self.target = (random.randint(1, NCOLS - 2), random.randint(1, NROWS - 2))
 
 
