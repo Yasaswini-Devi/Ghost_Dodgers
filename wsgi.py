86ba885258562506ba03c539ac4afe3473a47b24
Krishita Garg <krishitagarg@gmail.com>
2024-07-25 22:27:06 +0530

    Changed path to directory

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ import sys
 import os
 from flask import Flask
 
-sys.path.insert(0, '/home/KrishitaGarg/we_crafters_1/ghost-dodgers')
+sys.path.insert(0, '/home/KrishitaGarg/ghost-dodgers')
 
 from app import app as application
 
